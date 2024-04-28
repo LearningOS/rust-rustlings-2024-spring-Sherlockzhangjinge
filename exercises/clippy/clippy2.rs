645eddc9f328fc8eb7ec21bee8346fcbe5c37d4d
Sherlockzhangjinge <zjgzhangjinge@outlook.com>
2024-04-28 13:12:13 -0400

    update

--- exercises/clippy/clippy2.rs
+++ exercises/clippy/clippy2.rs
@@ -1,14 +1,12 @@
 // clippy2.rs
-// 
 // Execute `rustlings hint clippy2` or use the `hint` watch subcommand for a
-// hint.
-
-
+// hint
 fn main() {
     let mut res = 42;
-    let option = Some(12);
+    let mut option = Some(12);
     while let Some(x) = option {
         res += x;
+        option = None; // or any other way to make option None
     }
     println!("{}", res);
 }
